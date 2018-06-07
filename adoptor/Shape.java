aa1ab2152112696286eeec805818864b60ef79e8
Ainapurgaurav <gaurav.ainapur@gmail.com>
2018-06-07 19:59:03 +0530

    Update Shape.java

--- adoptor/Shape.java
+++ adoptor/Shape.java
@@ -6,7 +6,7 @@ public class Shape {
 	void setLocation(int a) {
 		System.out.println("location set at:"+a);
 	}
-	void getLocation(int a) {
+	void getLocation() {
 		System.out.println("enter location to set ");
 		int b=sc.nextInt();
 		setLocation(b);
